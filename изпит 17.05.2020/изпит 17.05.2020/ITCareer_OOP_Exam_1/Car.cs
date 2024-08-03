f8c7103ca5f364a149976a1453de0af6dc6e43a1
Velin Antonov <velin_antonov@abv.bg>
2024-08-03 18:43:00 +0300

    module 3

---
+++ изпит 17.05.2020/изпит 17.05.2020/ITCareer_OOP_Exam_1/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITCareer_OOP_Exam_1
+{
+    class Car
+    {
+        private int number;
+        private double price;
+        public int Number
+        {
+            get { return this.number; }
+            set { this.number = value; }
+        }
+        public double Price
+        {
+            get { return this.price; }
+            set 
+            {
+                if (value < 1000) { throw new ArgumentException("Invalid car price!"); }
+                this.price = value; 
+            }
+        }
+        public Car(int number, double price)
+        {
+            this.Number = number;
+            this.Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Car number {this.Number} costs {this.Price.ToString("F2")}";
+        }
+    }
+}
